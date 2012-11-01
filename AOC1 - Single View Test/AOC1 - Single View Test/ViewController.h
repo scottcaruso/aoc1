94aaeb1201c2f777e217e49cc6943355b7c3b531
Scott Caruso <scottcaruso@fullsail.edu>
2012-10-31 23:51:26 -0400

    Background color practice.

---
+++ AOC1 - Single View Test/AOC1 - Single View Test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AOC1 - Single View Test
+//
+//  Created by Scott Caruso on 10/31/12.
+//  Copyright (c) 2012 Scott Caruso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AOC1 - Single View Test/AOC1 - Single View Test/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  AOC1 - Single View Test
+//
+//  Created by Scott Caruso on 10/31/12.
+//  Copyright (c) 2012 Scott Caruso. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor cyanColor];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
