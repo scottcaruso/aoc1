6e41fb25bc18fa261f0c3bb22802e42e3ed643c3
Scott Caruso <scottcaruso@fullsail.edu>
2012-11-01 00:22:16 -0400

    Label practice.

--- AOC1 - Single View Test/AOC1 - Single View Test/ViewController.h
+++ AOC1 - Single View Test/AOC1 - Single View Test/ViewController.h
@@ -9,5 +9,11 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+{
+    UILabel *label1;
+    UILabel *label2;
+    UILabel *label3;
+    
+}
 
 @end
--- AOC1 - Single View Test/AOC1 - Single View Test/ViewController.m
+++ AOC1 - Single View Test/AOC1 - Single View Test/ViewController.m
@@ -16,9 +16,34 @@
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     self.view.backgroundColor = [UIColor cyanColor];
+    label1 = [[UILabel alloc] initWithFrame: CGRectMake(10.0f,10.0f,100.0f,20.0f)];
+    if (label1 != nil)
+    {
+        label1.text = @"Label text";
+        label1.textAlignment = NSTextAlignmentCenter;
+        label1.textColor = [UIColor blueColor];
+    }
+    label2 = [[UILabel alloc] initWithFrame: CGRectMake(10.0f,50.0f,100.0f,20.0f)];
+    if (label2 != nil)
+    {
+        label2.text = @"Second label";
+        label2.textAlignment = NSTextAlignmentCenter;
+        label2.textColor = [UIColor redColor];
+    }
+    label3 = [[UILabel alloc] initWithFrame: CGRectMake(10.0f,100.0f,100.0f,20.0f)];
+    if (label3 != nil)
+    {
+        label3.text = @"Third label";
+        label3.textAlignment = NSTextAlignmentCenter;
+        label3.textColor = [UIColor yellowColor];
+    }
+    
+    [self.view addSubview:label1];
+    [self.view addSubview:label2];
+    [self.view addSubview:label3];
+    [super viewDidLoad];
 }
 
 - (void)didReceiveMemoryWarning
