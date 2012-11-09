d5a0c46fd0b93be2819897839c621e60fe11bd8c
Scott Caruso <scottcaruso@fullsail.edu>
2012-11-08 20:51:21 -0500

    Steps 5 and 6

--- AOC1 - Week 3 Practice/AOC1 - Week 3 Practice/ViewController.m
+++ AOC1 - Week 3 Practice/AOC1 - Week 3 Practice/ViewController.m
@@ -22,22 +22,20 @@
     NSString *stringOne = @"This is not the ";
     NSString *stringTwo = @"string to use.";
     
-    //Create a function called Add. This function will take two NSInteger or int types and return the result of an addition between these two.
-    int resultValue = [self Add:integerOne secondValue:integerTwo];
-
-    //Create a BOOL function called Compare that takes two NSInteger values. Return YES or NO based on whether the values are equal.
-    BOOL compareResult = [self Compare:integerOne secondValue:integerTwo];
-    
-    //Create a function called Append. This function will take two NSStrings and return a new NSString containing the appended strings using an NSMutableString and the Append method.
-    //Call the Append function with two NSStrings. Capture the result and display a UIAlertView with the appended string using displayAlertWithString.
+    //Step 4 - Call the Append function with two NSStrings. Capture the result and display a UIAlertView with the appended string using displayAlertWithString.
     NSString *stringResult = [self Append:(stringOne) secondString:(stringTwo)];
-    
-    //This is the part that calls the alertView.
     UIAlertView *stringAlert = [[UIAlertView alloc] initWithTitle:@"Alert View Test" message: stringResult delegate: nil cancelButtonTitle:@"OK" otherButtonTitles: nil];
     if (stringAlert != nil)
     {
         [stringAlert show];
     }
+    
+    //Step 6 - Call the Add function passing in two integer values. Capture the return of this function into a variable.
+    int resultValue = [self Add:integerOne secondValue:integerTwo];
+    
+    
+    
+    BOOL compareResult = [self Compare:integerOne secondValue:integerTwo];
     
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
@@ -49,14 +47,14 @@
     // Dispose of any resources that can be recreated.
 }
 
-//This is the Add function that is called in viewDidLoad.
+//Step 1 - Create a function called Add. This function will take two NSInteger or int types and return the result of an addition between these two.
 -(int)Add:(NSInteger)valueOne secondValue:(NSInteger)valueTwo
 {
     int resultValue = valueOne + valueTwo;
     return resultValue;
 }
 
-//This is the Compare function that is called in viewDidLoad.
+//Step 2 - Create a BOOL function called Compare that takes two NSInteger values. Return YES or NO based on whether the values are equal.
 -(BOOL)Compare:(NSInteger)valueOne secondValue:(NSInteger)valueTwo
 {
     if (valueOne == valueTwo)
@@ -69,12 +67,18 @@
     }
 }
 
-//This is the Append function that is called in viewDidLoad.
+//Step 3 - Create a function called Append. This function will take two NSStrings and return a new NSString containing the appended strings using an NSMutableString and the Append method.
 -(NSString *)Append:(NSString *)stringOne secondString:(NSString *)stringTwo
 {
     NSMutableString *newString = [[NSMutableString alloc] initWithString: stringOne];
     [newString appendString:(stringTwo)];
     return newString;
+}
+
+//Step 5 - Create a function called DisplayAlertWithString. This function will take as a parameter an NSString.
+-(void)DisplayAlertWithString:(NSString *)stepFiveString
+{
+    UIAlertView *stringAlert = [[UIAlertView alloc] initWithTitle:@"Alert View Test" message: stringResult delegate: nil cancelButtonTitle:@"OK" otherButtonTitles: nil]; 
 }
 
 @end
